c06f83b87634d7972d66032f618d8c5ea34a6ce0
Sanjeev Sharma <ssharma2418@outlook.com>
2023-10-14 12:43:01 +0530

    done

---
+++ nike-clone-project/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+   <div>
+    <Navigation />
+    <Hero />
+   </div>
+
+  );
+}
+
+export default App;
