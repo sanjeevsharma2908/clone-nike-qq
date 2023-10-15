653a36594ce179f84db953eef526ace7b23aa4a1
Sanjeev Sharma <ssharma2418@outlook.com>
2023-10-15 22:31:37 +0530

    useState props and value processing

---
+++ project-2/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import ContactHeader from './components/ContactHeader/ContactHeader';
+import ContactForm from './components/ContactForm/ContactForm';
+
+function App() {
+  return (
+     <div>
+       <Navigation />
+       <ContactHeader />
+       <ContactForm />
+     </div>
+  );
+}
+
+export default App;
